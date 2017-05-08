5bea4d6af6a9ce9137596c1b983066eca13024d5
Ralph L <lettau.ralph@gmail.com>
2017-05-08 16:49:50 +0200

    Inital commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    
+    console.log(log);
+    fs.appendFile('server.log', log+'\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next()
+}); //middleware
+
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');    
+// }) //Middleware is setup in the order it is written
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name: 'Jesus',
+    //     likes: [
+    //         'Being Good',
+    //         'Not being curcified'
+    //     ]
+
+    // })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: `Somebody once told me the world is gonna roll me.`
+    })
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    })
+});
+
+// /bad -send back json
+app.get('/bad', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    res.send({
+        name: 'Tits',
+        likes: [
+            'Being Thcik',
+            'Bouncing'
+        ]
+
+    })
+});
+
+
+app.listen(3000, () => {
+    console.log('Server alive is on port 3000');
+});
